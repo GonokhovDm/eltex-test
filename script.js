4f9b37ee13314a9624812079919c47aee96e5ed3
Gonokhov Dmitry <gonoxoff@yandex.ru>
2023-07-16 23:15:09 +0700

    is done

--- script.js
+++ script.js
@@ -1,13 +1,81 @@
+//anchors
+const anchors = document.querySelectorAll('a[data-goto]');
+if(anchors) {
+  anchors.forEach(item => {
+    item.addEventListener('click', onMenuClick);
+  });
+
+  function onMenuClick(e) {
+    const anchor = e.target;
+    console.log(anchor)
+    if(anchor.dataset.goto && document.querySelector(anchor.dataset.goto)) {
+      const gotoBlock = document.querySelector(anchor.dataset.goto);
+      const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('.nav').offsetHeight - 20;
+      
+      window.scrollTo({
+        top: gotoBlockValue,
+        behavior: "smooth"
+      });
+      e.preventDefault();
+    }
+  }
+}
+
+//menu Burger
+const iconMenu = document.querySelector('.burger');
+if (iconMenu) {
+	const bgDark = document.querySelector('.bg-dark');
+  const wrapper = document.querySelector('.wrapper')
+	iconMenu.addEventListener("click", function (e) {
+		bgDark.classList.toggle('active');
+    wrapper.classList.toggle('active');
+
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+	});
+}
+
+function onClickHideMenu() {
+	const bgDark = document.querySelector('.bg-dark');
+  const wrapper = document.querySelector('.wrapper')
+	if ((bgDark) && (wrapper)) {
+		bgDark.classList.toggle('active');
+    wrapper.classList.toggle('active');
+	}
+}
+
+//swiper init
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
-  loop: true,
-  slidesPerView: 3,
+  loop: false,
+  slidesPerView: 1,
   spaceBetween: 16,
+  breakpoints: {
+    1440: {
+      slidesPerView: 3
+    },
+    768: {
+      slidesPerView: 2
+    }
+  },
 
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   }
+});
+
+//swiper card on hover
+let cardItems = document.querySelectorAll('.courses__cards-item')
+cardItems.forEach((element, index) => {
+  element.addEventListener('mouseenter', function() {
+    element.classList.add('active');
+  })
+  element.addEventListener('mouseleave', function() {
+    element.classList.remove('active');
+  })
 });
